6bbab5b1243676a99553774ce2ae3fbac2e01f67
Jinsoo-MackbookAir <ljslim27@gmail.com>
2025-11-12 16:04:57 +0900

    chore(react): StrictMode 비활성화

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode> /* StrictMode 비활성화 (학습용) */}
     <App />
-  </StrictMode>,
+  // </StrictMode>,
 )
